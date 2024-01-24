f340659daa03d825ecb496e2e7fa385d3274eb7c
Hugo Faria <hugofaria157@live.com>
2024-01-24 18:31:04 -0300

    removendo anotacoes de http status

--- src/main/java/dev/hugofaria/algafood/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/dev/hugofaria/algafood/domain/exception/EntidadeEmUsoException.java
@@ -3,7 +3,6 @@ package dev.hugofaria.algafood.domain.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends RuntimeException {
 
     public EntidadeEmUsoException(String mensagem) {
--- src/main/java/dev/hugofaria/algafood/domain/exception/NegocioException.java
+++ src/main/java/dev/hugofaria/algafood/domain/exception/NegocioException.java
@@ -3,7 +3,6 @@ package dev.hugofaria.algafood.domain.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NegocioException extends RuntimeException {
 
     public NegocioException(String mensagem) {
