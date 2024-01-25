ffcc86283921e408813a2e78404ebfa40000cbc4
Hugo Faria <hugofaria157@live.com>
2024-01-24 22:00:27 -0300

    tratando a exceção NoHandlerFoundException

--- src/main/java/dev/hugofaria/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/dev/hugofaria/algafood/api/exceptionhandler/ProblemType.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
-    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+    RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
 
@@ -17,5 +18,4 @@ public enum ProblemType {
         this.uri = "https://algafood.com.br" + path;
         this.title = title;
     }
-
 }
