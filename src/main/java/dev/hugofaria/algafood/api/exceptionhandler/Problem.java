a389fc98338e3f2af6734055620fb773a04e9c04
Hugo Faria <hugofaria157@live.com>
2024-01-24 23:07:30 -0300

    estendendo o formato do problema

--- src/main/java/dev/hugofaria/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/dev/hugofaria/algafood/api/exceptionhandler/Problem.java
@@ -1,17 +1,22 @@
 package dev.hugofaria.algafood.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Builder;
 import lombok.Getter;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+import java.time.LocalDateTime;
+
+@JsonInclude(Include.NON_NULL)
 @Getter
 @Builder
 public class Problem {
 
     private Integer status;
+    private LocalDateTime timestamp;
     private String type;
     private String title;
     private String detail;
+    private String userMessage;
 
 }
