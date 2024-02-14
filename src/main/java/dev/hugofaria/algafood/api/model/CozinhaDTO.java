26cf19cfbb44d912da6429a5468e3e447f1676e3
Hugo Faria <hugofaria157@live.com>
2024-02-14 01:04:24 -0300

    adicionando padrao dto

---
+++ src/main/java/dev/hugofaria/algafood/api/model/CozinhaDTO.java
@@ -0,0 +1,12 @@
+package dev.hugofaria.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CozinhaDTO {
+
+    private Long id;
+    private String nome;
+}
