19dc914c5a7fbf9cf5ccb9f9c6df86c2cce11c93
Hugo Faria <hugofaria157@live.com>
2024-02-14 22:14:39 -0300

    adicionando flush para finalizar pendencias no banco de dados

--- src/main/java/dev/hugofaria/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/dev/hugofaria/algafood/domain/service/CadastroCozinhaService.java
@@ -4,19 +4,19 @@ import dev.hugofaria.algafood.domain.exception.CozinhaNaoEncontradaException;
 import dev.hugofaria.algafood.domain.exception.EntidadeEmUsoException;
 import dev.hugofaria.algafood.domain.model.Cozinha;
 import dev.hugofaria.algafood.domain.repository.CozinhaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@AllArgsConstructor
 public class CadastroCozinhaService {
 
     private static final String MSG_COZINHA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
 
-    @Autowired
-    private CozinhaRepository cozinhaRepository;
+    private final CozinhaRepository cozinhaRepository;
 
     @Transactional
     public Cozinha salvar(Cozinha cozinha) {
@@ -27,6 +27,7 @@ public class CadastroCozinhaService {
     public void excluir(Long cozinhaId) {
         try {
             cozinhaRepository.deleteById(cozinhaId);
+            cozinhaRepository.flush();
 
         } catch (EmptyResultDataAccessException e) {
             throw new CozinhaNaoEncontradaException(cozinhaId);
