918265c71d126fe4cd27a8af25d8c06ff04246c8
Hugo Faria <hugofaria157@live.com>
2024-03-12 21:12:09 -0300

    implementando upload de arquivo com multipart/form-data

---
+++ src/main/java/dev/hugofaria/algafood/api/model/input/FotoProdutoInput.java
@@ -0,0 +1,14 @@
+package dev.hugofaria.algafood.api.model.input;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+public class FotoProdutoInput {
+
+	private MultipartFile arquivo;
+	private String descricao;
+	
+}
