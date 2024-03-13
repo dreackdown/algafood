221d7057ffff0d0d866a17ba21d3cb952ddf6fcd
Hugo Faria <hugofaria157@live.com>
2024-03-12 22:21:16 -0300

    anotation para validar content type e tamanho maximo do arquivo

--- src/main/java/dev/hugofaria/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/dev/hugofaria/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.nio.file.Path;
 import java.util.UUID;
 
@@ -18,11 +19,12 @@ import java.util.UUID;
 public class RestauranteProdutoFotoController {
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput) {
+    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
 
-        var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+        var nomeArquivo = UUID.randomUUID().toString()
+                + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
 
-        var arquivoFoto = Path.of("/Users/hugof/OneDrive/Desktop", nomeArquivo);
+        var arquivoFoto = Path.of("/Users/hugof/OneDrive/Desktop/catalogo", nomeArquivo);
 
         System.out.println(fotoProdutoInput.getDescricao());
         System.out.println(arquivoFoto);
--- src/main/java/dev/hugofaria/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/dev/hugofaria/algafood/api/model/input/FotoProdutoInput.java
@@ -1,14 +1,25 @@
 package dev.hugofaria.algafood.api.model.input;
 
+import dev.hugofaria.algafood.core.validation.FileContentType;
+import dev.hugofaria.algafood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class FotoProdutoInput {
 
-	private MultipartFile arquivo;
-	private String descricao;
-	
+    @NotNull
+    @FileSize(max = "500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
+    private MultipartFile arquivo;
+
+    @NotBlank
+    private String descricao;
+
 }
