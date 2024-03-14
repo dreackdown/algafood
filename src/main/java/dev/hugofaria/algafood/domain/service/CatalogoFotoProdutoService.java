63eee0fe76c83c73eca8bd5abacb3c35214a313a
Hugo Faria <hugofaria157@live.com>
2024-03-13 23:47:43 -0300

    implementando o servico de armazenagem de fotos no disco local

--- src/main/java/dev/hugofaria/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/dev/hugofaria/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -2,10 +2,12 @@ package dev.hugofaria.algafood.domain.service;
 
 import dev.hugofaria.algafood.domain.model.FotoProduto;
 import dev.hugofaria.algafood.domain.repository.ProdutoRepository;
+import dev.hugofaria.algafood.domain.service.FotoStorageService.NovaFoto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 @Service
@@ -14,16 +16,29 @@ public class CatalogoFotoProdutoService {
 
     private final ProdutoRepository produtoRepository;
 
+    private final FotoStorageService fotoStorage;
+
     @Transactional
-    public FotoProduto salvar(FotoProduto foto) {
+    public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
         Long restauranteId = foto.getRestauranteId();
         Long produtoId = foto.getProduto().getId();
+        String nomeNovoArquivo = fotoStorage.gerarNomeArquivo(foto.getNomeArquivo());
 
         Optional<FotoProduto> fotoExistente = produtoRepository
                 .findFotoById(restauranteId, produtoId);
 
         fotoExistente.ifPresent(produtoRepository::delete);
 
-        return produtoRepository.save(foto);
+        foto.setNomeArquivo(nomeNovoArquivo);
+        foto = produtoRepository.save(foto);
+        produtoRepository.flush();
+
+        NovaFoto novaFoto = FotoStorageService.NovaFoto.builder()
+                .nomeAquivo(foto.getNomeArquivo())
+                .inputStream(dadosArquivo)
+                .build();
+
+        fotoStorage.armazenar(novaFoto);
+        return foto;
     }
 }
---
+++ src/main/java/dev/hugofaria/algafood/infrastructure/storage/StorageException.java
@@ -0,0 +1,13 @@
+package dev.hugofaria.algafood.infrastructure.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+}
